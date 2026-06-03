80232c5c388d4d52d720668ab3d4bde38c8953b4
King <548635581@qq.com>
2026-06-03 11:09:16 +0800

    添加在排序数组中查找元素的第一个和最后一个位置（二分查找）解法

---
+++ Sort/在排序数组中查找元素的第一个和最后一个位置(二分查找).cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector <int> res(2);
+        if(nums.empty())
+        {
+            res[0] = -1;
+            res[1] = -1;
+            return res;
+        }
+        int first = lower_bound(nums.begin(), nums.end(),target) - nums.begin();
+        int second = upper_bound(nums.begin(), nums.end(),target) - nums.begin() - 1;
+        if(first >= nums.size() || nums[first] != target)
+        {
+            res[0] = -1;
+            res[1] = -1;
+        }
+        else
+        {
+            res[0] = first;
+            res[1] = second;
+        }
+        return res;
+    }
+};
