0d4b8f68b73dcc6f4cd87da121dde36e7ebf9f52
King <548635581@qq.com>
2026-07-08 09:40:46 +0800

    Add: 代码添加注释说明

--- Data Structure/链表/K个一组反转链表plus版.cpp
+++ Data Structure/链表/K个一组反转链表plus版.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 typedef struct LinkList{
     struct LinkList *next;
-    string data[3];   
+    string data[3];   // [0]:地址, [1]:值, [2]:下一个地址
 }ListNode, *List;
 
+// 根据输入数据构建链表，按地址链接节点
 void Create_Link(List &L, vector<vector<string>> &a, map<string,int> &addr2idx, int f){
-    L = new ListNode();                 
+    L = new ListNode();                  // 创建头节点
     L->data[0] = a[f][0];
     L->data[1] = a[f][1];
     L->data[2] = a[f][2];
     L->next = nullptr;                  
     ListNode* cur = L;                  
-    string nextAddr = a[f][2];
+    string nextAddr = a[f][2];           // 当前节点的下一个地址
 
-    while(nextAddr != "-1"){            
-        int idx = addr2idx[nextAddr];   
+    while(nextAddr != "-1"){             // 直到链表末尾
+        int idx = addr2idx[nextAddr];    // 通过地址映射找到下一节点的索引
         cur->next = new ListNode();
         cur = cur->next;
         cur->data[0] = a[idx][0];
@@ -26,22 +27,25 @@ void Create_Link(List &L, vector<vector<string>> &a, map<string,int> &addr2idx, 
     }
 }
 
+// K个一组反转链表
 ListNode* reverseKGroup(ListNode* head, int k) {
     
     int i{k};
-    ListNode* pre;
-    ListNode* next;
-    ListNode* res;
-    ListNode *begin, *tail;
+    ListNode* pre;    // 已反转部分的前驱
+    ListNode* next;   // 暂存下一节点
+    ListNode* res;    // 结果链表头
+    ListNode *begin, *tail;  // 当前组的头和尾
     bool first{true};
     while(head)
     {
+        // 检查剩余节点是否够k个
         ListNode* temp = head;
         while(i--){ if(!temp) return res; temp = temp->next; }
         i = k;
         if(first) tail = head;
         begin = head;
         pre = nullptr;
+        // 反转当前k个节点
         while(i--){
             if(!head) break;
             next = head->next;
@@ -50,6 +54,7 @@ ListNode* reverseKGroup(ListNode* head, int k) {
             head = next;
         }
         i = k;
+        // 将上一组的尾连接到当前组的头
         if(!first) tail->next = pre;
         tail = begin;
         begin->next = head;
@@ -68,16 +73,16 @@ int main(){
     cin>>start>>n>>k;
     for(auto i = 1; i <= n; ++i){
         cin>>a[i][0]>>a[i][1]>>a[i][2];
-        addr2idx[a[i][0]] = i;
-        if(a[i][0] == start)f = i;
+        addr2idx[a[i][0]] = i;             // 地址 -> 数组索引映射
+        if(a[i][0] == start)f = i;         // 找到起始节点
     }
 
-    Create_Link(L, a, addr2idx, f);                     
+    Create_Link(L, a, addr2idx, f);        // 构建链表
     ListNode* head = L;                               
 
-    ListNode* res = reverseKGroup(head, k);
+    ListNode* res = reverseKGroup(head, k);  // K个一组反转
 
-    
+    // 输出结果
     ListNode* p = res;
     while(p != nullptr){
         if(p->next != nullptr)
--- Sort/奖学金.cpp
+++ Sort/奖学金.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 struct Student {
-    int id;
-    int scores[3];
-    int total;
+    int id;         // 学号
+    int scores[3];  // 语数英三科成绩
+    int total;      // 总分
 };
 
 int main() {
@@ -14,23 +14,24 @@ int main() {
     vector<Student> students(n);
     
     for (int i = 0; i < n; ++i) {
-        students[i].id = i + 1;
+        students[i].id = i + 1;      // 学号从1开始
         students[i].total = 0;
         for (int j = 0; j < 3; ++j) {
             cin >> students[i].scores[j];
-            students[i].total += students[i].scores[j];
+            students[i].total += students[i].scores[j];  // 累加总分
         }
     }
     
+    // 按总分降序排序，总分相同则按语文成绩降序
     sort(students.begin(), students.end(), [](const Student& a, const Student& b) {
         if (a.total != b.total) 
             return a.total > b.total;
         return a.scores[0] > b.scores[0];
     });
     
-    int top = min(5, n);
+    int top = min(5, n);   // 取前5名
     for (int i = 0; i < top; ++i) {
-        cout << students[i].id << " " << students[i].total << endl;
+        cout << students[i].id << " " << students[i].total << endl;  // 输出学号和总分
     }
     
     return 0;
