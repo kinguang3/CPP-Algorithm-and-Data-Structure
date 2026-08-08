ec2b4baff3aa4f46b3dd34c1a076193dd0e481aa
King <548635581@qq.com>
2026-08-08 17:15:47 +0800

    Add election student council solution

---
+++ Sort/选举学生会.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<int> a(m);
+    for(int i=0;i<m;++i)cin>>a[i];
+    sort(a.begin(),a.end());
+    for(int i=0;i<m;++i)cout<<a[i]<<' ';
+}
